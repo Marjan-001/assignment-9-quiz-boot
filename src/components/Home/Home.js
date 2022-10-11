4fd72e277dc09767090455d0c40c7124347380eb
Marjan-001 <marjan199721@gmail.com>
2022-10-12 01:07:31 +0600

    hompage design

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Topics from '../Topics/Topics';
+import './Home.css'
 
 const Home = () => {
+    const { data } = useLoaderData();
+    console.log(data)
     return (
-        <div>
+        <div className='home'>
 
-            <h1 className='font-bold '> Welcome to QuizzBOOT</h1>
-            <p>Assessment, instruction, and practice that motivate every student to mastery</p>
+            <div>
+                <h1 className> Welcome to <span className='site-name'>QuizzBoot</span></h1>
+                <p>Assessment, instruction, and practice that motivate every student to mastery</p>
+                <div className='topic-container'>
+                    {
+                        data.map(topics => <Topics key={topics.id} topics={topics}></Topics>)
+                    }
+                </div>
+            </div>
 
         </div>
     );
