e54b758707a514ec5cd04b2fdc3cf4a60ef7891c
Marjan-001 <marjan199721@gmail.com>
2022-10-12 18:32:22 +0600

    errorpage

--- src/components/QuizDetails/QuizDetails.js
+++ src/components/QuizDetails/QuizDetails.js
@@ -3,20 +3,15 @@ import React from 'react';
 // import 'react-toastify/dist/ReactToastify.css';
 
 const QuizDetails = ({ qu }) => {
-    const { id, correctAnswer, options, question } = qu;
-
-
-
-    const handleCorrectAnswer = (e) => {
-
-        console.log(e.target.value);
+    const { correctAnswer, options, question } = qu;
 
 
 
 
-
-
-
+    const handleCorrectAnswer = (value, correctAnswer) => {
+        if (value === correctAnswer) {
+            console.log('match');
+        }
 
 
 
@@ -37,28 +32,28 @@ const QuizDetails = ({ qu }) => {
                     <ul className="items-center mt-6 w-full text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
                         <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
                             <div className="flex items-center pl-3">
-                                <input onClick={(e) => handleCorrectAnswer(e.target.value)} id="horizontal-list-radio-license" type="radio" value={options[0]} name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                <input onClick={() => handleCorrectAnswer()} id="horizontal-list-radio-license" type="radio" value='' name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
 
                                 <label for="horizontal-list-radio-license" className="py-3 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300">{options[0]} </label>
                             </div>
                         </li>
                         <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
                             <div className="flex items-center pl-3">
-                                <input onClick={() => handleCorrectAnswer()} id="horizontal-list-radio-id" type="radio" value="" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                <input onClick={() => handleCorrectAnswer()} id="horizontal-list-radio-id" type="radio" value='' name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
 
                                 <label for="horizontal-list-radio-id" className="py-3 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300">{options[1]} </label>
                             </div>
                         </li>
                         <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
                             <div className="flex items-center pl-3">
-                                <input onClick={() => handleCorrectAnswer()} id="horizontal-list-radio-millitary" type="radio" value="" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                <input id="horizontal-list-radio-millitary" type="radio" value="" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
 
                                 <label for="horizontal-list-radio-millitary" className="py-3 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300">{options[2]} </label>
                             </div>
                         </li>
                         <li className="w-full dark:border-gray-600">
                             <div className="flex items-center pl-3">
-                                <input onClick={() => handleCorrectAnswer()} id="horizontal-list-radio-passport" type="radio" value="" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                <input id="horizontal-list-radio-passport" type="radio" value="" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
 
                                 <label for="horizontal-list-radio-passport" className="py-3 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300">{options[3]} </label>
                             </div>
--- src/utilites/routes.js
+++ src/utilites/routes.js
@@ -4,15 +4,18 @@ import Main from "../layouts/Main"
 import Statistic from "../components/Statistic/Statistic"
 import Blog from "../components/Blog/Blog"
 import Quiz from "../components/Quiz/Quiz";
+import ErrorPage from "../components/ErrorPage";
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <Main></Main>,
+        errorElement: <ErrorPage />,
         children: [
             {
                 path: '/',
                 loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
+
                 element: <Home></Home>
             },
 
